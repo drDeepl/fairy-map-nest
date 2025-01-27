0e5aae4728464f2e2d0b7c8f86c81f52a57258eb
drDeepl <vlimaster@yandex.ru>
2025-01-27 15:47:49 +0700

    fix(common\dto\response): api.paginated.response

--- src/app/modules/story/dto/story/response/story-book-with-audios.response.dto.ts
+++ src/app/modules/story/dto/story/response/story-book-with-audios.response.dto.ts
@@ -1,13 +1,50 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { AudioResponseDto } from '../../audio-story/response/audio-response.dto';
-import { StoryBookResponseDto } from './story-with-img.response.dto';
 
-export class StoryBookWithAudiosResponseDto extends StoryBookResponseDto {
-  @ApiProperty({ description: 'одобренные озвучки для сказки' })
+import { EthnicGroupDto } from '@/app/modules/ethnic-group/dto/EthnicGroupDto';
+import { IsInt, IsNotEmpty } from 'class-validator';
+
+export class StoryBookWithAudiosResponseDto {
+  @ApiProperty({ description: 'id сказки', nullable: false })
+  @IsInt()
+  id: number;
+
+  @ApiProperty({ description: 'название истории', nullable: false })
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: 'id этнической группы',
+    type: () => EthnicGroupDto,
+    nullable: false,
+  })
+  ethnicGroup: EthnicGroupDto;
+
+  @ApiProperty({
+    description: 'url для получения обложки сказки',
+    nullable: true,
+  })
+  srcImg: string | null;
+
+  @ApiProperty({
+    description: 'текст сказки',
+    nullable: true,
+  })
+  text: string;
+
+  @ApiProperty({
+    description: 'одобренные озвучки для сказки',
+    type: () => AudioResponseDto,
+    isArray: true,
+  })
   audios: AudioResponseDto[];
 
   constructor(dto: Partial<StoryBookWithAudiosResponseDto>) {
-    super(dto as StoryBookResponseDto, dto.srcImg);
+    this.id = dto.id;
+    this.name = dto.name;
+    this.ethnicGroup = dto.ethnicGroup;
+    this.srcImg = dto.srcImg;
+    this.text = dto.text;
     this.audios = dto.audios;
   }
 }
--- src/common/dto/response/api-paginated.response.dto.ts
+++ src/common/dto/response/api-paginated.response.dto.ts
@@ -6,7 +6,7 @@ export const ApiPaginatedResponse = <TModel extends Type<any>>(
   model: TModel,
 ) => {
   return applyDecorators(
-    ApiExtraModels(PageResponseDto),
+    ApiExtraModels(PageResponseDto, model),
     ApiOkResponse({
       description: 'Успешное получение данных',
       schema: {
